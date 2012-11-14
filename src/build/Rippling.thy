887469bf396fa8ffd2aebb0e7b2bd5db1ae07c39
Gudmund Grov <gudmundgrov@gmail.com>
2012-11-14 11:30:46 +0000

    rippling stuff added

---
+++ src/build/Rippling.thy
@@ -0,0 +1,14 @@
+(* simple test of proof representation *)
+theory Rippling                                               
+imports        
+  Eval                                          
+uses
+  "../rtechn/rippling/rulesets/substs.ML"
+  "../rtechn/rippling/rulesets/bckimps.ML" 
+  "../rtechn/rippling/rulesets/fwdimps.ML" 
+begin
+
+end
+
+
+
