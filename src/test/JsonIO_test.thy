60d9923387208bd484e807dfba1f2b76b4247884
Gudmund Grov <gudmundgrov@gmail.com>
2013-04-10 10:52:17 +0100

    some comments on json rep added

--- src/test/JsonIO_test.thy
+++ src/test/JsonIO_test.thy
@@ -1,31 +1,35 @@
 theory JsonIO_test
-imports "../build/RTechn"
-uses "../rtechn/rtechn_json.ML"   
+imports "../build/RTechn"   
+uses "../rtechn/rtechn_json.ML"     
 begin
 ML{*
-local open RTechn Json in
+open RTechnJSON;
 
 val r = RTechn.id
             |> RTechn.set_name "assumption"
             |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm,"atac"));
 
-
    val test_src = StrName.NSet.of_list ["abc", "bbc"]
    val test_dest = StrNameNSet_to_json test_src |> StrNameNSet_from_json;
 
-   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> TClass;
+   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> RTechn.TClass;
    val test_dest = tac_assms_to_json test_src |> tac_assms_from_json;
 
 
-   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> TClass |> (fn x => Tactic (x,"test"));
+   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> RTechn.TClass |> (fn x => RTechn.Tactic (x,"test"));
    val test_dest = atomic_to_json test_src |> atomic_from_json;
 
 
-   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> TClass |> (fn x => Tactic (x,"test")) |> Appf;
+   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> RTechn.TClass |> (fn x => RTechn.Tactic (x,"test")) |> RTechn.Appf;
    val test_dest = appfn_to_json test_src |> appfn_from_json;
-   val r = RTechn {name = "test", appf = test_src};
+   val r = RTechn.id
+           |> RTechn.set_name "myname" 
+           |> RTechn.set_appf test_src;
+*}
 
-end
+ML{*
+ rtechn_to_json r
+ |> Json.string_of
 *}
 
 
