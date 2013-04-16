c805e44641075d3cf4bdcad20093f720699536c6
Gudmund Grov <gudmundgrov@gmail.com>
2013-04-16 17:57:25 +0100

    worked on combinators and a uniform represent of all graph related stuff (to avoid relying on isabelle context)

--- src/build/Graph.thy
+++ src/build/Graph.thy
@@ -2,15 +2,23 @@
 theory Graph                                               
 imports        
   RTechn               
-  GoalTyp                                       
-  "~~/contrib/quantomatic/core/isabelle/QuantoCore"                                             
+  GoalTyp                                         
+  "~~/contrib/quantomatic/core/isabelle/QuantoCore"                                            
 uses
-  "../graph/substdata.ML"  
+  "../graph/substdata.ML"          
   "../graph/vertex.ML"                         
   "../graph/edge.ML"  
-  "../graph/graph.ML"                  
+  "../graph/graph.ML"                     
   "../graph/theory.ML" 
-  "../graph/io.ML"                          
+  "../graph/io.ML"        
+
+  (* auxiliary functions (should be possibly for any graph) *)
+ (*  "../graph/graph_util.ML" *) 
+  "../graph/graph_comb.ML"   
+ 
+  (* proof strategy graphs *)     
+  "../psgraph/psgraph.ML"
+  "../psgraph/psgraph_comb.ML"             
 begin
 
 end
