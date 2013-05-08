e2d04e7b4d46b16d37f853d1b0200b3a5f436f32
Yuhui Lin <lyhlbsl@gmail.com>
2013-05-08 17:41:08 +0100

    clean up rtechn

--- src/provers/isabelle/test/JsonIO_test.thy
+++ src/provers/isabelle/test/JsonIO_test.thy
@@ -6,24 +6,24 @@ ML{*
 open RTechnJSON;
 
 val r = RTechn.id
-            |> RTechn.set_name "assumption"
-            |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm,"atac"));
+            |> RTechn.set_name (RT.mk "assumption")
+            |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm,(RT.mk "atac")));
 
-   val test_src = StrName.NSet.of_list ["abc", "bbc"]
-   val test_dest = StrNameNSet_to_json test_src |> StrNameNSet_from_json;
+   val test_src =  ["abc", "bbc"] |> map RT.mk |> RT.NSet.of_list
+   val test_dest = RTNSet_to_json test_src |> RTNSet_from_json;
 
-   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> RTechn.TClass;
+   val test_src =  ["abc", "bbc"] |> map RT.mk |> RT.NSet.of_list |> RTechn.TClass;
    val test_dest = tac_assms_to_json test_src |> tac_assms_from_json;
 
 
-   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> RTechn.TClass |> (fn x => RTechn.Tactic (x,"test"));
+   val test_src = test_src |> (fn x => RTechn.Tactic (x, RT.mk "test"));
    val test_dest = atomic_to_json test_src |> atomic_from_json;
 
 
-   val test_src = StrName.NSet.of_list ["abc", "bbc"] |> RTechn.TClass |> (fn x => RTechn.Tactic (x,"test")) |> RTechn.Appf;
+   val test_src = test_src |> RTechn.Appf;
    val test_dest = appfn_to_json test_src |> appfn_from_json;
    val r = RTechn.id
-           |> RTechn.set_name "myname" 
+           |> RTechn.set_name (RT.mk "myname") 
            |> RTechn.set_appf test_src;
 *}
 
--- src/provers/isabelle/test/rtechn_test.thy
+++ src/provers/isabelle/test/rtechn_test.thy
@@ -5,13 +5,13 @@ begin
 (*a simple rtechn*)
 ML{*
  val asm_rtechn = RTechn.id
-            |> RTechn.set_name "assumption"
-            |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm,"atac"));
+            |> RTechn.set_name (RT.mk "assumption")
+            |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm,RT.mk "atac"));
 *}
 
 ML{*
  val impI_rtechn = RTechn.id
-            |> RTechn.set_name "rule impI"
-            |> RTechn.set_atomic_appf (RTechn.Rule (StrName.NSet.single "impI"))
+            |> RTechn.set_name (RT.mk "rule impI")
+            |> RTechn.set_atomic_appf (RTechn.Rule (RT.NSet.single (RT.mk "impI")))
 *}
 end
