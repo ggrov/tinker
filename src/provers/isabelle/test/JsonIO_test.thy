58b8c3c3cb59c714175cdd20fec8076a095f0c9a
Yuhui Lin <lyhlbsl@gmail.com>
2013-05-15 16:29:22 +0100

    1) update TODO 2) rename 3) json for data

--- src/provers/isabelle/test/JsonIO_test.thy
+++ src/provers/isabelle/test/JsonIO_test.thy
@@ -1,7 +1,9 @@
 theory JsonIO_test
-imports "../build/RTechn"   
-uses "../../../rtechn/rtechn_json.ML"     
+imports "../build/RTechn" "../build/GoalTyp"   
+uses "../../../rtechn/rtechn_json.ML"   "../isaPrf.ML"
 begin
+
+(* reasoning technique *)
 ML{*
 open RTechnJSON;
 
@@ -32,5 +34,49 @@ ML{*
  |> Json.string_of
 *}
 
+
+(* goal type *)
+ML{*
+structure GTData : GT_DATA = GTDataFun (IsaPrf);
+structure Class : CLASS = ClassFun (GTData);
+structure Link : LINK = LinkFun(Class);
+structure GoalTyp = GoalTypFun(Link);
+structure GTJson = GTJsonFun (GoalTyp);
+*}
+
+ML{*
+val d1 = GTData.Int 0;
+val d2 = GTData.Position [1,2,3];
+val d3 = GTData.Term @{term " a + b"};
+
+Syntax.string_of_term @{context} @{term" a + b"} |> Syntax.parse_term @{context};
+
+GTJson.data_to_json @{context} d1 |> Json.pretty |> Pretty.writeln;
+GTJson.data_to_json @{context} d2 |> Json.pretty |> Pretty.writeln;
+GTJson.data_to_json @{context} d3 |> Json.pretty |> Pretty.writeln;
+
+GTJson.data_to_json @{context} d1 |> GTJson.data_from_json @{context} ;
+GTJson.data_to_json @{context} d2 |> GTJson.data_from_json @{context} ;
+GTJson.data_to_json @{context} d3 |> GTJson.data_from_json @{context} ;
+*}
+
+ML{*
+  fun data_ll_to_json ctxt dll =
+  let 
+    fun list_to_json dl = map (fn x => GTJson.data_to_json ctxt x) dl |> Json.Array
+  in
+    map list_to_json dll |> Json.Array
+  end
+    
+  fun data_ll_from_json ctxt  (Json.Array j) = map (fn (Json.Array x) => map (GTJson.data_from_json ctxt) x) j
+  | data_ll_from_json _ _ = raise EXP_PARSING_JSON "Not a Json array type."
+
+  val d1 = GTData.Int 0;
+  val d2 = GTData.Position [1,2,3];
+  val d3 = GTData.Int 2;
+  
+  data_ll_to_json @{context} [[d1, d2],[d3]] |> Json.pretty |> Pretty.writeln;
+  data_ll_to_json @{context} [[d1, d2],[d3]] |> data_ll_from_json @{context} 
+*}
 
 end
