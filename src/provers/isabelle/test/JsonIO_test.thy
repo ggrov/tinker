c2c3f25fc922657ffda75643bb206150904e0670
Yuhui Lin <lyhlbsl@gmail.com>
2013-05-16 14:31:58 +0100

    RT to C & string in rtechn_json

--- src/provers/isabelle/test/JsonIO_test.thy
+++ src/provers/isabelle/test/JsonIO_test.thy
@@ -9,16 +9,12 @@ open RTechnJSON;
 
 val r = RTechn.id
             |> RTechn.set_name (RT.mk "assumption")
-            |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm,(RT.mk "atac")));
+            |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm,( "atac")));
 
-   val test_src =  ["abc", "bbc"] |> map RT.mk |> RT.NSet.of_list
-   val test_dest = RTNSet_to_json test_src |> RTNSet_from_json;
-
-   val test_src =  ["abc", "bbc"] |> map RT.mk |> RT.NSet.of_list |> RTechn.TClass;
+   val test_src =  ["abc", "bbc"] |> map C.mk |> C.NSet.of_list |> RTechn.TClass;
    val test_dest = tac_assms_to_json test_src |> tac_assms_from_json;
 
-
-   val test_src = test_src |> (fn x => RTechn.Tactic (x, RT.mk "test"));
+   val test_src = test_src |> (fn x => RTechn.Tactic (x,  "test"));
    val test_dest = atomic_to_json test_src |> atomic_from_json;
 
 
