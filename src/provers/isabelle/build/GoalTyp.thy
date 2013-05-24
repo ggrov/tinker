46ef95af66da4ca61576d9b52325a72f8e4ccc52
Yuhui Lin <lyhlbsl@gmail.com>
2013-05-24 15:24:30 +0100

    check in match

--- src/provers/isabelle/build/GoalTyp.thy
+++ src/provers/isabelle/build/GoalTyp.thy
@@ -1,17 +1,19 @@
 (* simple test of proof representation *)
-theory GoalTyp                             
+theory GoalTyp 
 imports                
  "~~/contrib/isaplib/isabelle/isaplib/isaplib"                                           
 uses
+  "../../prover.ML"  
+  "../../atomic.ML"
   "../../../rtechn_names.ML"   
-  "../../../provers/prover.ML"  
-  "../../../goaltype/goaltyp_data.ML" 
+  "../../../goaltype/goaltyp_data.ML"
+  "../../match_param.ML"
   "../../../goaltype/class.ML"  
   "../../../goaltype/link.ML"
  "../../../goaltype/goaltyp.ML"    
  "../../../goaltype/goaltyp_json.ML"
- (* "../../../goaltype/gnode.ML"  *)
-(* "../../../goaltype/gt_match.ML"  *)
+  "../../../goaltype/gnode.ML"  
+ "../../../goaltype/goaltyp_match.ML"
 (*
  "../isaPrf.ML"
  "../init.ML"
