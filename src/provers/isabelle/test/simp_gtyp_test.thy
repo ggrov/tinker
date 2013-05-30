b9fc64779d4fcfe60179e5b1dfe377cb14997573
Yuhui Lin <lyhlbsl@gmail.com>
2013-05-30 14:20:24 +0100

    more simple goal type

--- src/provers/isabelle/test/simp_gtyp_test.thy
+++ src/provers/isabelle/test/simp_gtyp_test.thy
@@ -1,6 +1,6 @@
 theory simp_gtyp_test      
 imports                   
-  "../../basic_isabelle/build/BIsaP"  
+  "../../basic_isabelle/build/BIsaP"   
 
   uses "../../../goaltype/simple/gnode.ML"
        "../../../goaltype/goaltyp_data.ML"
@@ -9,34 +9,25 @@ imports
        "../../../goaltype/class.ML"
        "../../../goaltype/simple/goaltyp_json.ML"  
        "../../../goaltype/simple/goaltyp_match.ML"
+       "../../../goaltype/simple/goaltyp_i.ML"
+       "../isa_match_param.ML"
 begin
-ML{*
-Pretty.str o G.string_of_name;
-G.name_ord;
-G.name_eq
-*}
+
 ML{*
 structure GoalTypData : GOALTYP_DATA = GoalTypDataFun (IsaProver);
 structure Class : CLASS = ClassFun (GoalTypData);
 structure Link : LINK = LinkFun(structure GoalTypData = GoalTypData structure Prover = IsaProver);
-structure GoalTyp = GoalTypFun(structure Link = Link structure Class = Class);
-structure GoalTypJson = GoalTypJsonFun  (structure GoalTyp: GOAL_TYP = GoalTyp
-                                         structure Link : LINK = Link
-                                         structure Class : CLASS = Class
-                                         structure GoalTypData : GOALTYP_DATA = GoalTypData
-                                         structure Prover : PROVER = IsaProver);
 structure MatchParam = IsaMatchParamFun (structure GoalTypData = GoalTypData
                                          structure Prover = IsaProver )
-structure GoalTypMatch = GoalTypMatchFun (structure GoalTyp: GOAL_TYP = GoalTyp
-                                         structure Link : LINK = Link
+structure GoalTypMatch = GoalTypMatchFun (structure Link : LINK = Link
                                          structure Class : CLASS = Class
                                          structure GoalTypData : GOALTYP_DATA = GoalTypData
                                          structure Prover : PROVER = IsaProver
                                          structure Atomic = BIsaAtomic
                                          structure MatchParam = MatchParam);
-structure BasicGoalTyp = BasicGoalTypFun (structure GoalTyp = GoalTyp
-                                          structure Atomic : ATOMIC = BIsaAtomic
-                                          structure GoalTypJson : GOALTYP_JSON = GoalTypJson
-                                          structure GoalTypMatch : GOALTYP_MATCH = GoalTypMatch)
+structure BasicGoalTyp = BasicGoalTypFun (structure Atomic = BIsaAtomic
+                                          structure GoalTypMatch =  GoalTypMatch
+                                          structure GoalTypJson = GoalTypJson); 
+
 *}
 
