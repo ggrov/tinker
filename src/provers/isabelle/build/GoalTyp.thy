b59327c0d50b1cd937b70ba10e9e0b8e1ada27b5
Yuhui Lin <lyhlbsl@gmail.com>
2013-05-31 12:06:58 +0100

    fix json format to enable scala drawing

--- src/provers/isabelle/build/GoalTyp.thy
+++ src/provers/isabelle/build/GoalTyp.thy
@@ -1,18 +1,29 @@
 (* simple test of proof representation *)
-theory GoalTyp
+theory GoalTyp 
 imports                
  "basic/BasicIsaPS"                                            
 uses
-  "../../../goaltype/full/gnode.ML"
+
   "../../../goaltype/goaltyp_data.ML"
   "../../match_param.ML"
   "../../../goaltype/class.ML"  
   "../../../goaltype/link.ML"
- "../../../goaltype/full/goaltyp.ML"    
- "../../../goaltype/full/goaltyp_json.ML"  
- "../../../goaltype/full/goaltyp_match.ML"
- "../../../goaltype/full/goaltyp_i.ML"
- 
+
+(* for full goaltyp *)
+
+  "../../../goaltype/full/goaltyp.ML"  
+  "../../../goaltype/full/gnode.ML"  
+  "../../../goaltype/full/goaltyp_json.ML"  
+  "../../../goaltype/full/goaltyp_match.ML"
+  "../../../goaltype/full/goaltyp_i.ML"
+
+(* for simple goaltyp *)
+(*
+  "../../../goaltype/simple/gnode.ML"
+  "../../../goaltype/simple/goaltyp_json.ML"  
+  "../../../goaltype/simple/goaltyp_match.ML"
+  "../../../goaltype/simple/goaltyp_i.ML"
+*)
 begin
 
 end
--- src/provers/isabelle/test/comb_test.thy
+++ src/provers/isabelle/test/comb_test.thy
@@ -1,11 +1,48 @@
 (* simple test of combinators *)
-theory comb_test                                                
+theory comb_test                                              
 imports               
- "../build/Prf"                                                                  
+ "../build/GoalTyp"                                                                  
+  uses  "../isa_prover.ML" "../../basic_isabelle/isa_atomic.ML" "../isa_match_param.ML" (* "../isa_setup.ML"*)
 begin
+ML{*
+structure GoalTypData : GOALTYP_DATA = GoalTypDataFun (IsaProver);
+structure Class : CLASS = ClassFun (GoalTypData);
+structure Link : LINK = LinkFun(structure GoalTypData = GoalTypData structure Prover = IsaProver);
+structure GoalTyp = GoalTypFun(structure Link = Link structure Class = Class);
+structure GoalTypJson = GoalTypJsonFun  (structure GoalTyp: GOAL_TYP = GoalTyp
+                                         structure Link : LINK = Link
+                                         structure Class : CLASS = Class
+                                         structure GoalTypData : GOALTYP_DATA = GoalTypData
+                                         structure Prover : PROVER = IsaProver);
+structure MatchParam = IsaMatchParamFun (structure GoalTypData = GoalTypData
+                                         structure Prover = IsaProver )
+structure GoalTypMatch = GoalTypMatchFun (structure GoalTyp: GOAL_TYP = GoalTyp
+                                         structure Link : LINK = Link
+                                         structure Class : CLASS = Class
+                                         structure GoalTypData : GOALTYP_DATA = GoalTypData
+                                         structure Prover : PROVER = IsaProver
+                                         structure Atomic = BIsaAtomic
+                                         structure MatchParam = MatchParam);
+structure BasicGoalTyp = BasicGoalTypFun (structure GoalTyp = GoalTyp
+                                          structure Atomic : ATOMIC = BIsaAtomic
+                                          structure GoalTypJson : GOALTYP_JSON = GoalTypJson
+                                          structure GoalTypMatch : GOALTYP_MATCH = GoalTypMatch)
+*}
 
 ML{*
-;
+structure Theory = PSTheoryFun(structure GoalTyp = BasicGoalTyp);
+structure PSGraph = PSGraphFun(structure PSTheory = Theory
+                               structure Atomic = BIsaAtomic);
+structure PSComb = PSCombFun(PSGraph);
+structure EData = EDataFun(structure Atomic = BIsaAtomic
+                           structure PSGraph = PSGraph);
+*}
+
+ML{*
+structure EVal = EValFun(EData);
+*}   
+
+ML{*
  val impI = RTechn.id
            |> RTechn.set_name (RT.mk "impI")
            |> RTechn.set_atomic_appf (RTechn.Rule (StrName.NSet.single "HOL.impI"))
@@ -28,12 +65,10 @@ ML{*
 *}
 
 ML{*
- 
- PS_GraphicalTheoryIO.OutputGraphJSON.output g
+ Theory.PS_TheoryIO.OutputGraphJSON.output g
   |> Json.pretty
   |> Pretty.writeln;
 *}
-
 
 
 
