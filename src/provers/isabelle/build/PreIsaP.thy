36f08dbf930fa481ca6b76c674eaf6987abfb1a0
Gudmund Grov <gudmundgrov@gmail.com>
2013-06-13 14:22:51 +0100

    started to look at parsing

---
+++ src/provers/isabelle/build/PreIsaP.thy
@@ -0,0 +1,14 @@
+(* simple test of proof representation *)
+theory PreIsaP                                                   
+imports           
+  Main                                                               
+begin
+
+ML{*
+ structure IsaXML = XML;
+*}
+
+end
+
+
+
