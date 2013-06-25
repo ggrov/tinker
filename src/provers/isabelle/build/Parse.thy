def21547aa57e0d67c471c92ef22a01dc34d2ce6
Gudmund Grov <gudmundgrov@gmail.com>
2013-06-25 11:58:44 +0100

    whym parsing example added

--- src/provers/isabelle/build/Parse.thy
+++ src/provers/isabelle/build/Parse.thy
@@ -8,12 +8,6 @@ begin
  ML_file "../../../parse/whym_tree.ML"  
  ML_file "../../../parse/whym_parse.ML" 
 
- -- "simple test"
- ML{*
-   val whymtree = WhyMTree.parse_file "/u1/staff/gg112/psgraph/src/parse/examples/simple2.yxml";
-   val graph =  WhyMParse.graph_of_goal @{context} (K[]) whymtree;
-   PSGraph.PSTheory.write_dot "/u1/staff/gg112/pptest.dot" graph
- *}
 end
 
 
