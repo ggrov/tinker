b1110396c5eb0148bb33bfc650659f14866f3c92
Gudmund Grov <gudmundgrov@gmail.com>
2013-07-30 11:29:16 +0100

    limited support for evaluation strategies added

--- src/provers/isabelle/build/basic/Eval.thy
+++ src/provers/isabelle/build/basic/Eval.thy
@@ -4,10 +4,10 @@ imports
   PSGraph                                                                          
 begin         
                                                
-  ML_file "../../../../eval/eval_data.ML"                                               
-  ML_file "../../../../eval/eval_atomic.ML"                                                                               
-  ML_file "../../../../eval/eval_graph.ML"                                    
-  ML_file "../../../../eval/eval.ML"                                    
+  ML_file "../../../../eval/eval_data.ML"                                                    
+  ML_file "../../../../eval/eval_atomic.ML"                                                                                
+  ML_file "../../../../eval/eval_graph.ML"                                     
+  ML_file "../../../../eval/eval.ML"                                                     
 
 end
 
