ae0920734f79973ff6f1f5e1d3ecb7cf2d8030b3
Yuhui Lin <lyhlbsl@gmail.com>
2013-09-04 14:40:14 +0100

    1) debug message for class matching 2) add prop in top_level

--- src/provers/isabelle/test/feature_test.thy
+++ src/provers/isabelle/test/feature_test.thy
@@ -4,6 +4,147 @@ imports
  "../build/IsaP"                                                                  
 begin
 
+ML{* 
+  exception feature_test_exp of term
+  structure GTD = GoalTypData;  structure Atomic = IsaAtomic;
+  structure TF = TermFeatures;
+  fun trm_to_thm thy trm = Thm.cterm_of thy trm |>  Thm.trivial
+*}
+
+-- "aux functions"
+ML {*
+fun gtd_to_string (GTD.String str) = str;
+fun gtds_to_string xs = SOME (map gtd_to_string xs) handle _ => NONE;
+
+fun gtd_to_term (GTD.Term trm) = trm;
+fun gtds_to_terms xs = SOME (map gtd_to_term xs) handle _ => NONE;
+
+
+*}
+
+-- "has symbols"
+ML{*
+(* we accept non-well-formedness *)
+fun has_symbols' (_:Proof.context)  _ [] = true 
+ |  has_symbols' (_:Proof.context) (Atomic.Fact thm) xs = 
+     (case gtds_to_string xs of
+       NONE => false
+      | SOME xs' => TermFeatures.has_constants xs' (Thm.prop_of thm))
+ | has_symbols' (_:Proof.context) (Atomic.Concl thm) xs = 
+     (case gtds_to_string xs of
+       NONE => false
+      | SOME xs' => TermFeatures.has_constants xs' (Thm.prop_of thm))
+ | has_symbols' _  _ _ = false;
+
+        
+(*
+fun has_symbols (_:Proof.context) _ [] = false
+ |  has_symbols ctxt obj (x::xs) = 
+     (has_symbols' obj x) orelse (has_symbols ctxt obj xs);
+*)
+
+val test = has_symbols' @{context} (Atomic.Concl(trm_to_thm @{theory}  @{prop "a \<and> b \<longrightarrow> c"})) [(GTD.String "HOL.implies"), (GTD.String "HOL.conj")];
+val test = has_symbols' @{context} (Atomic.Concl (trm_to_thm @{theory} @{prop "a \<longrightarrow> b"})) [(GTD.String "HOL.implies"), (GTD.String "HOL.conj")];
+*}
+setup {* IsaMatchParam.add_class_feature (F.mk "has_symbols",has_symbols') *}
+
+
+-- "top symbol"
+ML{*
+
+(* will it always be a prop? *)
+fun top_symbol' (_:Proof.context)  (Atomic.Fact thm) xs = 
+     (case gtds_to_string xs of
+       NONE => false
+      | SOME xs' => exists (fn str => TermFeatures.is_top_level str  ( (Thm.prop_of thm))) xs')
+ |  top_symbol' (ctxt:Proof.context)  (Atomic.Concl thm) xs = 
+    let val _ = Pretty.writeln (Syntax.pretty_term ctxt  ( (Thm.concl_of thm))) in
+     (case gtds_to_string xs of
+       NONE => ( writeln "no in topsym" ; false)
+      | SOME xs' =>  exists (fn str => TermFeatures.is_top_level str (  (Thm.concl_of thm))) xs') end
+ | top_symbol' (_:Proof.context)  _ _ = false (* or true *)
+
+(*
+fun top_symbols (_:Proof.context) _ [] = false
+ |  top_symbols ctxt thm (x::xs) = 
+     (top_symbol' thm x) orelse (top_symbols ctxt thm xs);
+*)
+val test = top_symbol' @{context} (Atomic.Concl ( trm_to_thm @{theory} @{prop "a --> b "})) [(GTD.String "HOL.implies"), (GTD.String "HOL.conj")];
+val test = top_symbol' @{context} (Atomic.Concl ( trm_to_thm @{theory}@{prop "a \<longrightarrow> b"})) [(GTD.String "HOL.conj")];
+
+*}
+setup {* IsaMatchParam.add_class_feature (F.mk "top_symbols",top_symbol') *}
+
+-- "shape: example where context is required"
+ML {*
+fun is_shape' ctxt (Atomic.Fact thm) xs = 
+     (case gtds_to_terms xs of
+       NONE => false
+      | SOME xs' => forall (fn trm => TermFeatures.is_shape (Proof_Context.theory_of ctxt) trm (Thm.prop_of thm)) xs')
+  | is_shape' ctxt (Atomic.Concl thm) xs = 
+     (case gtds_to_terms xs of
+       NONE => false
+      | SOME xs' => forall (fn trm => TermFeatures.is_shape (Proof_Context.theory_of ctxt) trm (Thm.prop_of thm)) xs')
+      | is_shape' _ _ _ = false (* or true *)
+ 
+(* fun is_shape (_:Proof.context) _ [] = false
+ |  is_shape ctxt thm (x::xs) = 
+     (is_shape' ctxt thm x) orelse (is_shape ctxt thm xs); *)
+*}
+setup {* IsaMatchParam.add_class_feature (F.mk "is_shape",is_shape') *}
+
+
+(* test matching for some goal types *)
+ML{*
+  val gt = FullGoalTyp.default;
+  val class_topsymb = GoalTyp.Class.add_item (F.mk "top_symbols") [[(GTD.String "HOL.conj")], [(GTD.String "HOL.conj"), (GTD.String "HOL.implies")]] Class.top;
+  val class_hassymb_topsymb = GoalTyp.Class.add_item (F.mk "has_symbols") [[(GTD.String "HOL.implies")]] class_topsymb;
+
+  val gt_demo = 
+    GoalTyp.set_gclass class_hassymb_topsymb gt
+    |> GoalTyp.set_facts [class_hassymb_topsymb, ]
+    |> GoalTyp.set_name (G.mk"has_symbol_and_top_symbol");
+
+  val auto = RTechn.id
+            |> RTechn.set_name (RT.mk "auto")
+            |> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm, "auto"));
+  val auto_tac = fn x => ( Clasimp.auto_tac x);
+
+  val psauto = PSComb.LIFT ([gt_demo],[]) (auto);
+  val psgraph = psauto PSGraph.empty;
+
+  val psgraph = 
+    psauto PSGraph.empty 
+    |> PSGraph.update_atomics (StrName.NTab.doadd ("auto", auto_tac));
+
+*}
+ML{*
+  val (pn,pp) = IsaAtomic.init @{context} (trm_to_thm @{theory} @{prop "A --> A"});
+  val pnode_tab = 
+       StrName.NTab.ins
+         (IsaAtomic.get_pnode_name pn,pn)
+         StrName.NTab.empty;
+  val edata_0 = EData.init psgraph pp pnode_tab [];
+
+  val t = Atomic.get_pnode_concl  pn |> Thm.concl_of ; (* todo: has a extrl porp on top ???? *)
+  
+  FullGoalTyp.init_lift gt_demo pn;
+*}
+
+ML{*
+  val edata0 = EVal.init psgraph @{context} (trm_to_thm @{theory} @{prop "A --> A"}) |> hd;
+  val edata1 = EVal.evaluate_any edata0 ;
+*}
+
+
+ML{*-
+UISocket.ui_eval JsonControllerProtocol'.run_in_textstreams (SOME edata0) (K edata0)
+*}
+
+
+-- "some previous codes"
+(*
+
 -- "storing data"
 ML{*
  structure GTD = GoalTyp.Class.GoalTypData;
@@ -21,76 +162,14 @@ ML{*
   val get_feature = F.NTab.lookup o get_features;
 *}
 
- -- "the class match function using this"
+-- "the class match function using this"
 ML{*
   fun class_data_match fname ctxt obj data = 
     case get_feature (Proof_Context.theory_of ctxt) fname of
        NONE => false
      | SOME f => f ctxt obj data;
 *}
-
--- "aux functions"
-ML {*
-fun gtd_to_string (GTD.String str) = str;
-fun gtds_to_string xs = SOME (map gtd_to_string xs) handle _ => NONE;
-
-fun gtd_to_term (GTD.Term trm) = trm;
-fun gtds_to_terms xs = SOME (map gtd_to_term xs) handle _ => NONE;
-
-
-*}
-
--- "has symbols"
-ML{*
-(* we accept non-well-formedness *)
-fun has_symbols' _ [] = true 
- |  has_symbols' thm xs = 
-     case gtds_to_string xs of
-       NONE => false
-      | SOME xs' => TermFeatures.has_constants xs' (Thm.prop_of thm);
-        
-
-fun has_symbols (_:Proof.context) _ [] = false
- |  has_symbols ctxt thm (x::xs) = 
-     (has_symbols' thm x) orelse (has_symbols ctxt thm xs);
-*}
-setup {* add_feature (F.mk "has_symbols",has_symbols) *}
-
--- "top symbol"
-ML{*
-TermFeatures.is_top_level;
-
-(* will it always be a prop? *)
-fun top_symbol' _ [] = false (* or true *)
- |  top_symbol' thm xs = 
-     case gtds_to_string xs of
-       NONE => false
-      | SOME xs' => exists (fn str => TermFeatures.is_top_level str (Thm.prop_of thm)) xs';
-        
-
-fun top_symbols (_:Proof.context) _ [] = false
- |  top_symbols ctxt thm (x::xs) = 
-     (top_symbol' thm x) orelse (top_symbols ctxt thm xs);
-*}
-setup {* add_feature (F.mk "top_symbols",top_symbols) *}
-
--- "shape: example where context is required"
-ML {*
-fun is_shape' _ _ [] = false (* or true *)
- |  is_shape' ctxt thm xs = 
-     case gtds_to_terms xs of
-       NONE => false
-      | SOME xs' => forall (fn trm => TermFeatures.is_shape (Proof_Context.theory_of ctxt) trm (Thm.prop_of thm)) xs';
- 
-fun is_shape (_:Proof.context) _ [] = false
- |  is_shape ctxt thm (x::xs) = 
-     (is_shape' ctxt thm x) orelse (is_shape ctxt thm xs); 
-*}
-setup {* add_feature (F.mk "is_shape",is_shape) *}
-
-
-(* TO DO : test matching for some goal types *)
-
+*)
 
 end
 
