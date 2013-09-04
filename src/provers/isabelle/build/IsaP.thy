b85e1a135baf6a0c89ce64692fd5f476910bfcb8
Yuhui Lin <lyhlbsl@gmail.com>
2013-09-04 10:54:07 +0100

    runtime feature register

--- src/provers/isabelle/build/IsaP.thy
+++ src/provers/isabelle/build/IsaP.thy
@@ -5,8 +5,8 @@ imports
 begin     
   
  ML_file "../../basic_isabelle/isa_prover.ML" 
- ML_file "../../basic_isabelle/isa_atomic.ML" 
- ML_file "../isa_match_param.ML"      
+ ML_file "../../basic_isabelle/isa_atomic.ML"      
+
  ML_file "../isa_setup.ML" 
 
 end
