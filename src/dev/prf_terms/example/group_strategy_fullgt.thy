8739eac5a85e6762dfe68ce744241ec2f526a998
Colin Farquhar <cif30@hw.ac.uk>
2013-11-06 16:06:06 +0000

    tweaks to strategy examples

--- src/dev/prf_terms/example/group_strategy_fullgt.thy
+++ src/dev/prf_terms/example/group_strategy_fullgt.thy
@@ -91,6 +91,11 @@ RTechn.id
 |> RTechn.set_name (RT.mk "subst ax3s")
 |> RTechn.set_atomic_appf (RTechn.Subst (StrName.NSet.of_list ["ax3s"]));
 
+val ax3sb2 =
+RTechn.id
+|> RTechn.set_name (RT.mk "subst(2) ax3s")
+|> RTechn.set_atomic_appf (RTechn.Subst (StrName.NSet.of_list ["ax3s"]));
+
 val ax2sb =
 RTechn.id
 |> RTechn.set_name (RT.mk "subst ax2s")
@@ -111,14 +116,16 @@ RTechn.id
 |> RTechn.set_name (RT.mk "rule refl")
 |> RTechn.set_atomic_appf (RTechn.Rule (StrName.NSet.of_list ["refl"]));
 
-val back =
+(*val back =
 RTechn.id
 |> RTechn.set_name (RT.mk "back")
-|> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TAllAsm, "back"));
+|> RTechn.set_atomic_appf (RTechn.Tactic (RTechn.TClass, "back"));*)
 *}
 
 
 -- "PSGraph"
+
+(* Combinators *)
 
 ML{*
  infixr 6 THENG;
@@ -132,20 +139,23 @@ ML{*
 (* id_rev sub-strategy *)
 
 ML{*
-val psax3s = PSComb.LIFT ([gt_id],[gt_inv]) (ax3sb)
-val psback = LIFT ([gt_inv],[gt_inv])(back)
-val psax3s_alt = psax3s THENG psback
+val psax3s = PSComb.LIFT ([gt_id],[gt_inv]) (ax3sb2)
+(*val psback = LIFT ([gt_inv],[gt_inv])(back)
+val psax3s_alt = psax3s THENG psback*)
 val psax2s = PSComb.LIFT ([gt_inv],[gt_inv]) (ax2sb)
 val psinv = PSComb.LIFT ([gt_inv], [gt_id]) (inv_revb)
 val psax1 = PSComb.LIFT ([gt_id], [gt]) (ax1b)
-val psf_idrev = psax3s_alt THENG psax2s THENG psinv THENG psax1;
+val psf_idrev = psax3s THENG psax2s THENG psinv THENG psax1;
 val psg_idrev = IsaMethod.init_psgraph psf_idrev @{context};
 val psgraph_idrev = IsaMethod.apply_psgraph_tac psg_idrev;
 *}
 
 ML{*
-val id_rev_h = NEST "ind" psf_idrev;
+val id_rev_h = NEST "inv_rev" psf_idrev;
 *}
+
+
+(* Basic strategy - gexp_id *)
 
 ML{*
 val psinduct = PSComb.LIFT ([gt_induct],[gt_base, gt_step]) (induct);
@@ -153,11 +163,11 @@ val psbase = PSComb.LIFT ([gt_base],[]) (simp1a);
 val psstep = PSComb.LIFT ([gt_step],[gt_id]) (simp2b);
 val psid = PSComb.LIFT ([gt_id],[gt]) (id_revb);
 val psasm = PSComb.LIFT ([gt],[]) (asm);
-val psf = psinduct THENG psbase THENG psstep THENG psid (*THENG psasm*);
+val psf_idorder = psinduct THENG psbase THENG psstep THENG psid THENG psasm;
 (*val psgraph_idorder =   psf PSGraph.empty 
       |> PSGraph.load_atomics (StrName.NTab.list_of (IsaMethod.get_tacs @{theory}));*)
 
-val psf_hier = psinduct THENG psbase THENG psstep THENG id_rev_h(* THENG psasm*);
+val psf_hier = psinduct THENG psbase THENG psstep THENG id_rev_h THENG psasm;
 
 *}
 
@@ -168,7 +178,7 @@ val psgraph_asm = IsaMethod.apply_psgraph_tac psg_asm;
 *}
 
 ML{*
-val psg_idorder = IsaMethod.init_psgraph psf @{context};
+val psg_idorder = IsaMethod.init_psgraph psf_idorder @{context};
 val psgraph_idorder = IsaMethod.apply_psgraph_tac psg_idorder;
 *}
 
@@ -178,13 +188,15 @@ val psgraph_idorder_hier = IsaMethod.apply_psgraph_tac psg_idorder_hier;
 *}
 
 ML{*
-val psf_step = psstep THENG psid (*THENG psasm*);
+val psf_step = psstep THENG psid THENG psasm;
 val psg_step = IsaMethod.init_psgraph psf_step @{context};
 val psgraph_idstep = IsaMethod.apply_psgraph_tac psg_step;
 *}
 
+(* Strategy with loops - gexp_Suc*)
+
 ML{*
-val ps_induct = LIFT ([gt_induct], [gt_base, gt_step]) (induct);
+val ps_induct = LIFT ([gt_induct],[gt_base, gt_step]) (induct);
 val ps_base = LIFT ([gt_base],[gt_ref]) (simp2b);
 val ps_step1 = LIFT ([gt_step],[gt_step]) (simp2b);
 val ps_step2 = LIFT ([gt_step],[gt_ref]) (simp2b);
@@ -200,17 +212,20 @@ val psgraph_Suc = IsaMethod.apply_psgraph_tac psg_Suc;
 (* Combined strategy *)
 
 ML{*
-val ps_induct = LIFT ([gt_induct],[gt_base,gt_step]) (induct);
+val ps_induct = LIFT ([gt_induct],[gt_base, gt_step]) (induct);
 val ps_base1 = LIFT ([gt_base],[]) (simp1a);
 val ps_step1 = LIFT ([gt_step],[gt_id]) (simp2b);
-val ps_step2 = LIFT ([gt_step,gt_step,gt_base],[gt_step,gt_ref]) (simp2b);
+val ps_step2 = LIFT ([gt_step,gt_step],[gt_step,gt_ref]) (simp2b);
 val ps_step_loop = LOOP ps_step2 gt_step;
+val ps_ref = LIFT ([gt_ref],[]) (refla);
+val ps_step_full = ps_step_loop THENG ps_ref;
+val ps_step_nest = NEST "looped subst" ps_step_full;
 val ps_base_choice = OR (ps_base1,ps_step_loop);
-val ps_step_choice = OR (ps_step1,ps_step_loop);
-val ps_id = LIFT ([gt_id],[gt]) (id_revb)
-val ps_ref = LIFT ([gt_ref],[]) (refla)
-val psf_combined = ps_induct THENG ps_base_choice THENG ps_step_choice 
-                    THENG ps_id THENG ps_ref;
+val ps_step_choice = OR (ps_step1,ps_step_nest);
+val ps_id = LIFT ([gt_id],[gt]) (id_revb);
+val ps_asm = LIFT ([gt],[]) (asm);
+val psf_combined = ps_induct THENG ps_base1 THENG ps_step_choice 
+                    THENG ps_id THENG ps_asm;
 val psg_combined = IsaMethod.init_psgraph psf_combined @{context};
 val psgraph_combined = IsaMethod.apply_psgraph_tac psg_combined;
 *}
@@ -221,8 +236,7 @@ val psgraph_combined = IsaMethod.apply_psgraph_tac psg_combined;
 -- "Examples"
 
 ML{*
-eval_interactive;
-val [edata] = EVal.init psg_Suc @{context} @{prop "gexp g n ** g = gexp g (Suc n)"};
+val [edata] = EVal.init psg_combined @{context} @{prop "gexp e n = e"};
 *}
 
 ML{*
@@ -233,8 +247,8 @@ ML{*
 EData.get_pplan edata;
 *}
 
-lemma "a ** e = a"
-  apply (tactic "psgraph_idrev @{context}")
+lemma "gexp e n = e"
+  apply (tactic "psgraph_combined @{context}")
 oops
 
 
@@ -244,12 +258,10 @@ lemma "gexp e n = e"
   apply (tactic "psgraph_idstep @{context}")
   apply assumption
   apply assumption
-  apply assumption
 done
 
 lemma "gexp e n = e"
   apply (tactic "psgraph_idorder @{context}")
-  apply assumption
   apply assumption
   apply assumption
 done
@@ -259,16 +271,30 @@ lemma "gexp e n = e"
   apply (rule l1)
   apply (subst l2)
  
-  apply (tactic "psgraph_idrev @{context}")
+ (* apply (tactic "psgraph_idrev @{context}")*)
 
-  apply (subst ax3s) 
-  back
+  apply (subst(2) ax3s) 
   apply (subst ax2s)
   apply (subst inv_rev)
   apply (subst ax1)
 
   apply assumption
 done
+
+
+lemma "gexp e n = e"
+  apply (induct n)
+  apply (rule l1)
+  apply (subst l2)
+  apply (subst ax3s)
+ML_prf{*
+Seq.chop
+*}
+  apply (subst ax2s)
+  apply (subst inv_rev)
+  apply (subst ax1)
+  apply assumption
+oops
 
 
 lemma "gexp e n = e"
@@ -402,5 +428,19 @@ ML{*
 val (EVal.Cont edata7) = EVal.evaluate_any edata6;
 val edata7 = EVal.normalise_gnode edata7;
 PSGraph.PSTheory.write_dot (path ^"hiergraph7.dot") (EData.get_graph edata7)   
+*}
+
+
+(* Combined Strategy *)
+
+ML{*
+val edata0 = EVal.init psg_combined @{context} @{prop "gexp e n = e"} |> hd; 
+PSGraph.PSTheory.write_dot (path ^ "combgraph0.dot") (EData.get_graph edata0)  
+*}
+
+ML{*
+val (EVal.Cont edata1) = EVal.evaluate_any edata0;
+val edata1 = EVal.normalise_gnode edata1;
+PSGraph.PSTheory.write_dot (path ^"combgraph1.dot") (EData.get_graph edata1)   
 *}
 end
