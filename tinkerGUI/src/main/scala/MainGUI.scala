d6995d315b8e40660958f1c47d1243cbaa01ca4f
plebras <pl196.hw@gmail.com>
2014-06-12 16:28:15 +0100

    created one API between Quantomatic and TinkerGUI (functionalities so far : created a graph, add vertex, add boundaries, add edges with or without source/target), created one Main class to test the API file

---
+++ tinkerGUI/src/main/scala/MainGUI.scala
@@ -0,0 +1,24 @@
+import scala.swing._
+import quantoLib.QuantoLibAPI
+// import graph._
+
+object MainGUI extends SimpleSwingApplication {
+	def top = new MainFrame{
+		title = "Tinker"
+		size = new Dimension(800,800)
+		minimumSize = new Dimension(400,400)
+		contents = QuantoLibAPI.makeGraph
+		QuantoLibAPI.addVertex(100, 100, "GN")
+		QuantoLibAPI.addVertex(200, 200, "RT")
+		QuantoLibAPI.addVertex(60, 200, "RT")
+		QuantoLibAPI.addVertex(200, 150, "GN")
+		QuantoLibAPI.addVertex(500, 130, "RT")
+		QuantoLibAPI.addVertex(-50, -90, "GN")
+		QuantoLibAPI.addBoundary(150, -30)
+		QuantoLibAPI.addBoundary(60, 210)
+		QuantoLibAPI.addEdge("v0", "v2")
+		QuantoLibAPI.addEdge("v4", "b0")
+		QuantoLibAPI.addEdge((245.0, 160.0), "v4")
+		QuantoLibAPI.addEdge((-75.0, -75.0), (85.0, 85.0))
+	}
+}
---
+++ tinkerGUI/src/main/scala/lib/QuantoLibAPI.scala
@@ -0,0 +1,135 @@
+package quantoLib
+
+import quanto._
+import quanto.util.json._
+import quanto.data._
+import quanto.data.Names._
+import quanto.gui._
+import quanto.gui.graphview._
+import scala.swing._
+
+/*
+*
+* API file for the library based on quantomatic
+*
+*/
+
+object QuantoLibAPI {
+	val graphPanel = new BorderPanel{
+		println("loading theory " + Theory.getClass.getResource("strategy_graph.qtheory"))
+		val theoryFile = new Json.Input(Theory.getClass.getResourceAsStream("strategy_graph.qtheory"))
+		val theory = Theory.fromJson(Json.parse(theoryFile))
+		val graphDoc = new GraphDocument(this, theory)
+		val graphView = new GraphView(theory, graphDoc)
+		val graphScrollPane = new ScrollPane(graphView)
+		add(graphScrollPane, BorderPanel.Position.Center)
+	}
+	var graph = graphPanel.graphDoc.graph
+	val theory = graphPanel.theory
+	val view = graphPanel.graphView
+
+	def makeGraph = graphPanel
+
+	def loadGraph {
+		
+	}
+	def saveGraph {
+
+	}
+
+	/**
+	  * Method to add a vertex to the graph.
+	  * @param : x, the x coordinate of the mouse
+	  * @param : y, the y coordinate of the mouse
+	  * @param : typ the type of hte vertex : "GN" or "RT"
+	  * @return : the name of the new vertex as a string
+	  */
+	def addVertex(x: Double, y: Double, typ: String) : String = {
+		val coord = view.trans fromScreen (x, y)
+		val vertexData = NodeV(data = theory.vertexTypes(typ).defaultData, theory = theory).withCoord(coord)
+		val vertexName = graph.verts.freshWithSuggestion(VName("v0"))
+		graphPanel.graphDoc.graph = graph.addVertex(vertexName, vertexData.withCoord(coord))
+		graph = graphPanel.graphDoc.graph
+		return(vertexName.s)
+	}
+
+	/**
+	  * Method to add a boundary to the graph.
+	  * @param : x, the x coordinate of the mouse
+	  * @param : y, the y coordinate of the mouse
+	  * @return : the name of the new boundary as a string
+	  */
+	def addBoundary(x: Double, y: Double) : String = {
+		val coord = view.trans fromScreen (x, y)
+		val vertexData = WireV(theory = theory, annotation = JsonObject("boundary" -> JsonBool(true)))
+		val vertexName = graph.verts.freshWithSuggestion(VName("b0"))
+		graphPanel.graphDoc.graph = graph.addVertex(vertexName, vertexData.withCoord(coord))
+		graph = graphPanel.graphDoc.graph
+		return(vertexName.s)
+	}
+
+	/**
+	  * Method to add an edge between two element of the graph.
+	  * @param : start, the source element
+	  * @param : end, the target element
+	  * @return : 
+	  */
+	def addEdge(start: String, end: String) {
+		val startV = VName(start)
+		val endV = VName(end)
+		val defaultData = DirEdge.fromJson(theory.defaultEdgeData, theory)
+		graphPanel.graphDoc.graph = graph.addEdge(graph.edges.fresh, defaultData, (startV, endV))
+		graph = graphPanel.graphDoc.graph
+		graph.edgesBetween(startV, endV).foreach { view.invalidateEdge }
+	}
+
+	/**
+	  * Method to add an edge targeting one element of the graph.
+	  * @param : startCoord, the coordinates where the edge starts, a boundary will be created as a source element
+	  * @param : end, the target element
+	  * @return : 
+	  */
+	def addEdge(startCoord: (Double, Double), end: String) {
+		val start = addBoundary(startCoord._1, startCoord._2)
+		addEdge(start, end)
+	}
+
+	/**
+	  * Method to add an edge coming from one element of the graph.
+	  * @param : start, the source
+	  * @param : endCoord, the coordinates where the edge ends, a boundary will be created as a target element
+	  * @return : 
+	  */
+	def addEdge(start: String, endCoord: (Double, Double)) {
+		val end = addBoundary(endCoord._1, endCoord._2)
+		addEdge(start, end)
+	}
+
+	/**
+	  * Method to add an edge in the graph.
+	  * @param : startCoord, the coordinates where the edge starts, a boundary will be created as a source element
+	  * @param : endCoord, the coordinates where the edge ends, a boundary will be created as a target element
+	  * @return : 
+	  */
+	def addEdge(startCoord: (Double, Double), endCoord: (Double, Double)) {
+		val start = addBoundary(startCoord._1, startCoord._2)
+		val end = addBoundary(endCoord._1, endCoord._2)
+		addEdge(start, end)
+	}
+
+	def addBBox {
+
+	}
+	def removeVertex {
+
+	}
+	def removeBoundary {
+
+	}
+	def removeEdge {
+
+	}
+	def removeBBox {
+
+	}	
+}
