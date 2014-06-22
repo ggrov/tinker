0207ce55396cc12b0024ea275a0de7761f8f1def
Gudmund Grov <gudmundgrov@gmail.com>
2014-06-22 17:03:44 +0100

    started to fix quanto encoding for new quantomatic and new rtechn (and simplifying goal type accordingly)

--- src/build/isabelle/Graph.thy
+++ src/build/isabelle/Graph.thy
@@ -2,18 +2,26 @@
 theory Graph                                                                                           
 imports        
   RTechn                          
-  BasicGoalTyp                                                                          
-  "~~/contrib/quantomatic/core/isabelle/QuantoCore"                                                         
+  BasicGoalTyp                                                                            
+  "~~/contrib/quantomatic/core/isabelle/QuantoCore"                                                           
 uses
-
   "../../debug_handler.ML"
-  "../../graph/graph_comb.ML" (* generic for graphs - move to quantomatic? *)
-  "../../graph/substdata.ML"                                
+
+  (* generic for graphs - move to quantomatic? *)
+  (* "../../graph/graph_comb.ML" *)
+
+  "../../graph/substdata.ML"                                  
   "../../graph/vertex.ML"                            
   "../../graph/edge.ML"  
   "../../graph/graph.ML"                     
   "../../graph/theory.ML"                                   
 begin
+
+ (* changes
+    Graph.EData.data -> Graph.edata
+    Graph.get_edge -> Graph.get_edge_info
+    Graph.get_vnames -> get_vertices 
+  *)
 
 end
 
