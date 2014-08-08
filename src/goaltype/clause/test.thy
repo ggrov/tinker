420a3ceabb1345374265445ea4c35a8e42ced4f9
ggrov <gudmundgrov@gmail.com>
2014-08-08 17:17:10 +0100

    more on new goal type

--- src/goaltype/clause/test.thy
+++ src/goaltype/clause/test.thy
@@ -1,37 +1,18 @@
 theory test
 imports
-  "../../build/isabelle/BasicGoalTyp"
+  "../../build/isabelle/BasicGoalTyp" 
   (* "../../build/isabelle/Eval"
   "../../provers/isabelle/basic/build/BIsaMeth"
   *)
 begin
 
-ML{*
- datatype Args = Name of string 
-                | Var of string
-                | Concl
-                | Hyps
-                | Term of string (* fix me: should we make this term? *)
-                | Clause of string * (Args list)
-*}
+  ML_file "goaltype.ML"                                                               
 
-ML{*
-   type pnode = { pname : string, pctxt : Proof.context, ptrm : Term.term } 
-    (* could also hold the thm? *)
-  type pplan = { goal : Thm.thm, opengs : pnode list, usedgs : StrName.NSet.T }
-  datatype gnode = GNode of 
-     { name : string,
-       env : Term.term StrName.NTab.T
-     }
-
-  type args = string list
-*}
-
-ML{*
-  type atomic = args -> pnode * pplan ->  gnode -> gnode list 
-*}
-
-
+ (*
+   f(X,Y) :- a(X,Y), b(X,Y).
+   f(g(X),Y) :- b(X),c(Y).
+   f(g(test),h(test2)).
+ *)
 
 (*
   top_symbol(Concl,X) := case lookup(X) of  get_top(Concl)
