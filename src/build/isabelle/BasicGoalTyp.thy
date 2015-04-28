f163dedce9bf92e837b8bf222fdfdbf166226f23
Yuhui Lin <lyhlbsl@gmail.com>
2015-04-28 22:24:36 +0100

    fix the loading error for isabelle 2014, which is caused by changed interface for eval ml string

--- src/build/isabelle/BasicGoalTyp.thy
+++ src/build/isabelle/BasicGoalTyp.thy
@@ -1,10 +1,17 @@
 (* simple test of proof representation *)
 theory BasicGoalTyp   
-imports LoadQuantoLib13       (*LoadQuantoLib*)                  
+imports LoadQuantoLib13      (*LoadQuantoLib *)            
 begin
   ML_file "../../rtechn_names.ML" 
   ML_file "../../provers/prover.sig.ML"   
-  ML_file "../../goaltype/basic_goaltyp.sig.ML"     
+  ML_file "../../goaltype/basic_goaltyp.sig.ML"  
+
+ML{*
+val a = 1;
+
+;
+a;
+*}   
 end
 
 
--- src/build/isabelle/LoadQuantoLib13.thy
+++ src/build/isabelle/LoadQuantoLib13.thy
@@ -6,5 +6,8 @@ imports
 
 begin 
 (*ML_file "lib/raw_source.ML"*)
-ML{* structure RawSource = Source *}
+ML{* 
+  structure RawSource = Source;
+  val eval_text = ML_Context.eval_text true (Position.start);
+*}
 end
