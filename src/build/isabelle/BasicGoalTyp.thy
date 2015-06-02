81b21bf05eebab9bf9299dae3d274f114423c20f
Yuhui Lin <lyhlbsl@gmail.com>
2015-06-02 17:25:10 +0100

    1) starting updating the core using the new prover and goal type sig (updated uptp PSGraph) 2) refactorying codes 3) merge the conv_tac branch

--- src/build/isabelle/BasicGoalTyp.thy
+++ src/build/isabelle/BasicGoalTyp.thy
@@ -1,17 +1,12 @@
 (* simple test of proof representation *)
 theory BasicGoalTyp   
-imports LoadQuantoLib      (*LoadQuantoLib *)            
+imports LoadQuantoLib      (*LoadQuantoLib13 *)            
 begin
+
   ML_file "../../rtechn_names.ML" 
   ML_file "../../provers/prover.sig.ML"   
   ML_file "../../goaltype/basic_goaltyp.sig.ML"  
 
-ML{*
-val a = 1;
-
-;
-a;
-*}   
 end
 
 
