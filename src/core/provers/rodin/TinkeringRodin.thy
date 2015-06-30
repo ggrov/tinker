01c612156ecd100b05a6e6216599a986c933c172
Yuhui Lin <lyhlbsl@gmail.com>
2015-06-30 12:55:26 +0100

    more works on Rodin prover

--- src/core/provers/rodin/TinkeringRodin.thy
+++ src/core/provers/rodin/TinkeringRodin.thy
@@ -4,6 +4,11 @@ imports
 begin 
 
 (* TODO: move rodin prover here *)
+ML_file "./interface/unicode_helper.ML"
+ML_file "./interface/rodin_socket.ml"
+ML_file "./interface/interface.ML" 
+ML_file "./rodin_prover.ML"
+ML_file "./interface/rodin_extra.ML"
 
 ML{*
   structure SimpleGoalType : BASIC_GOALTYPE = SimpleGoalType_Fun(structure Prover = RodinProver val struct_name = "SimpleGoalType");
@@ -22,3 +27,4 @@ ML{*
 *}
 
 ML{*  open Env_Tac_Lib  *}
+
