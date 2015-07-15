7c175272cb1a7bd2945eff929c8f10b46f930f44
Yuhui Lin <lyhlbsl@gmail.com>
2015-07-15 17:26:42 +0100

    refactor psgraph for the simplified son

--- src/core/build/isabelle/PSGraph.thy
+++ src/core/build/isabelle/PSGraph.thy
@@ -6,7 +6,7 @@ begin
 
   ML_file "../../psgraph/psgraph.sig.ML"        
   ML_file "../../psgraph/psgraph.ML"      
-  ML_file "../../psgraph/psgraph_comb.ML" 
+  (*ML_file "../../psgraph/psgraph_comb.ML" *)
 
 end                                
 
