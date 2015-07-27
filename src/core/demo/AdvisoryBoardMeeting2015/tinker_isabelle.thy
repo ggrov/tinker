f12181263482996d7a6efb1bae85234e4701bde8
Yuhui Lin <lyhlbsl@gmail.com>
2015-07-27 15:44:40 +0100

    1) SUPPORT EXCEPTION logging type 2) support exception cmd/rsp

--- src/core/demo/AdvisoryBoardMeeting2015/tinker_isabelle.thy
+++ src/core/demo/AdvisoryBoardMeeting2015/tinker_isabelle.thy
@@ -9,12 +9,12 @@ ML{*
   val ps = PSGraph.read_json_file (path ^"demo.psgraph");
 *}
 
-ML{* -
+ML{*-
   TextSocket.safe_close();
 *}   
 
-ML{*
-val thm = Tinker.start_ieval @{context} ps [] @{prop "P  \<longrightarrow> P \<longrightarrow>  (P \<and>  P \<and> (Q \<longrightarrow> Q))"} (* prove the goal *)
+ML{*   
+val thm = Tinker.start_ieval @{context} ps [] @{prop "P  \<longrightarrow>   (P \<and>  P \<and> (Q \<longrightarrow> Q))"} (* prove the goal *)
           |> EData.get_pplan |> IsaProver.get_goal_thm(* get the theorem *)
 *}
 
