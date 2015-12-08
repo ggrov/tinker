76ba3aa74bc12ce8bf6fcf9385ea7ec3318313ce
Yuhui Lin <yuhuilin@Yuhuis-MacBook-Pro.local>
2015-12-08 12:33:15 +0000

    move files to utils

--- src/core/build/isabelle/BasicGoalTyp.thy
+++ src/core/build/isabelle/BasicGoalTyp.thy
@@ -2,11 +2,10 @@
 theory BasicGoalTyp   
 imports LoadQuantoLib      (*LoadQuantoLib13 *)            
 begin
-
-  ML_file "../../unicode_helper.ML"
-  ML_file "../../psgraph_names.ML" 
-  ML_file "../../logging_handler.ML"
-  ML_file "../../ml_exec.ML"
+  ML_file "../../utils/unicode_helper.ML"
+  ML_file "../../utils/psgraph_names.ML" 
+  ML_file "../../utils/logging_handler.ML"
+  ML_file "../../utils/ml_exec.ML"
   ML_file "../../provers/prover.sig.ML"  
   ML_file "../../goaltype/basic_goaltyp.sig.ML"
 
