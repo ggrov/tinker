ee58f5c2c7c46ea70a9d37bfec2557faad0a526c
Yibo Liang <yibo.insane@gmail.com>
2016-02-02 22:19:02 +0000

    psgraph edit for rodin demo, deleted backup

--- src/rodin_plugin/tinker.for.rodin.backup/src/tinker/core/protocol/states/SocketStates.java
+++
@@ -1,9 +0,0 @@
-package tinker.core.protocol.states;
-
-public class SocketStates {
-	public static int LISTENING=0;
-	public static int DISCONNECTED=1;
-	public static int CONNECTED=2;
-	public static int SENDING_CMD=3;
-	public static int SENDING_CANCELLATION=4;
-}
