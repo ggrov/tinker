833cfb9f9cfe80bc3f2d63c6aadc01c4723b2dd3
Yuhui Lin <lyhlbsl@gmail.com>
2016-06-07 13:32:57 +0100

    support rippling

--- src/core/provers/isabelle/clausal/CIsaP.thy
+++ src/core/provers/isabelle/clausal/CIsaP.thy
@@ -8,7 +8,7 @@ begin
  ML_file "../../../goaltype/clause/goaltype.ML"                                                                                                                        
 
  -- "the prover"  
- ML_file "../basic/isa_prover.ML"                                
+ ML_file "../basic/isa_prover.ML"                              
 
  -- "setting up PSGraph"
 
