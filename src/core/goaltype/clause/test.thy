b2d69183aee9d51ccc3885f4b17e93aa7fd4c84e
Yuhui Lin <lyhlbsl@gmail.com>
2016-06-29 11:09:04 +0100

    support exclamation in atomic

--- src/core/goaltype/clause/test.thy
+++ src/core/goaltype/clause/test.thy
@@ -250,8 +250,12 @@ ML{*
 
   ML{*
    C.imatch data pnode ("top_symbol",[C.Concl,C.Name "conj"]);
-   C.type_check data pnode ("top_symbol",[C.Hyps,C.Name "conj"]);
+   C.type_check data pnode ("top_symbol",[C.Hyps,C.Name "disj"]);
   *}        
+ML{*
+(C.scan_goaltyp @{context} "!top_symbol(concl, imp)");
+   C.type_check data pnode (C.scan_goaltyp @{context} "!top_symbol(concl, imp)")
+*}
 
 ML{*
    val fdef2 = "not_top(X) :- not(top_symbol(X,conj)).";
