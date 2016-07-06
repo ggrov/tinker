56552b0485ab00bd7a3e67a1fd465287c87c7c1f
Yuhui Lin <lyhlbsl@gmail.com>
2016-07-06 17:20:54 +0100

    1) refactor and clean up examples for JFR 2) move jar from dev to exmaples 3) records evaluation and the refactoring process as JSON

--- src/core/build/isabelle/BasicGoalTyp.thy
+++ src/core/build/isabelle/BasicGoalTyp.thy
@@ -2,8 +2,8 @@
 theory BasicGoalTyp   
 imports LoadQuantoLib      (*LoadQuantoLib13 *)            
 begin
-  ML_file "../../utils/unicode_helper.ML"
   ML_file "../../utils/psgraph_names.ML" 
+  ML_file "../../utils/unicode_helper.ML"
   ML_file "../../utils/logging_handler.ML"
   ML_file "../../utils/ml_exec.ML"
   ML_file "../../provers/prover.sig.ML"  
