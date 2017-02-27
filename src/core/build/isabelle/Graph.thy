fdc7e41e5ff687715aa99ff262052b0e8640c04c
Yuhui Lin <lyhlbsl@gmail.com>
2017-02-27 16:32:44 +0000

    graph sig

--- src/core/build/isabelle/Graph.thy
+++ src/core/build/isabelle/Graph.thy
@@ -6,10 +6,13 @@ begin
   
 ML_file "../../graph/graph_data.sig.ML"          
 ML_file "../../graph/graph_data.ML" 
-ML_file "../../graph/env_tac_utils.ML"
 
 ML_file "../../graph/graph.sig.ML"
-  ML_file "../../graph/graph.ML"
+ML_file "../../graph/graph.ML"  
+ML_file "../../graph/graph_utils.ML"    
+ML_file "../../graph/env_tac_utils.ML"
+
+
   
   ML_file "../../graph/graph_io.sig.ML"    
   ML_file "../../graph/graph_io.ML"
